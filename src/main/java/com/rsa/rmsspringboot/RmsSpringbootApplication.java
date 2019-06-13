eb86caa646939b03c0d662173349618fa99912fe
Rai S. Arijasa <raisuardhyana.arijasa@mitrais.com>
2019-06-13 14:58:14 +0800

    init project

---
+++ src/main/java/com/rsa/rmsspringboot/RmsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rsa.rmsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsa/rmsspringboot/RmsSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rsa.rmsspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RmsSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
